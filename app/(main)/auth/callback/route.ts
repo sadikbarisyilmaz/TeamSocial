ff0cd2a9e7070e2d00ddf3e59dde94caf232bca5
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2026-02-18 01:28:58 +0300

    feat: OAuth option added

---
+++ app/(main)/auth/callback/route.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  // next is the path to redirect to after successful login
+  const next = searchParams.get("next") ?? "/";
+
+  if (code) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // Return the user to an error page if something goes wrong
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
