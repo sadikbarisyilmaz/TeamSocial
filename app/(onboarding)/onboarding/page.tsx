6ad48f53783fba5cc3fb13f45ad54f5ae809f1c0
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2026-02-17 12:02:25 +0300

    fix: moved protection logic inside onboarding.tsx component

--- app/(onboarding)/onboarding/page.tsx
+++ app/(onboarding)/onboarding/page.tsx
@@ -1,27 +1,7 @@
 import Onboarding from "@/components/onboarding";
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function Page() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
-    redirect("/auth/login");
-  }
-
-  const { data: profile } = await supabase
-    .from("profiles")
-    .select("team_id")
-    .eq("id", data?.claims?.sub)
-    .single();
-
-  // if user does belong to a team
-  if (profile?.team_id) {
-    return redirect("/");
-  }
-
   return (
     <Suspense>
       <Onboarding />
--- components/onboarding.tsx
+++ components/onboarding.tsx
@@ -4,12 +4,22 @@ import OnboardingSteps from "./onboarding-steps";
 
 export default async function Onboarding() {
   const supabase = await createClient();
-  const { data, error } = await supabase.auth.getClaims();
 
+  const { data, error } = await supabase.auth.getClaims();
   if (error || !data?.claims) {
     redirect("/auth/login");
   }
-  // return JSON.stringify(data.claims, null, 2);
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("team_id")
+    .eq("id", data?.claims?.sub)
+    .single();
+
+  // if user does belong to a team
+  if (profile?.team_id) {
+    return redirect("/");
+  }
 
   return <OnboardingSteps />;
 }
